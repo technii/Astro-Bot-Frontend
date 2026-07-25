4267ddcd04894e04bcb4c7311e8797c3170e121f
techni <145453745+technii@users.noreply.github.com>
2026-07-25 18:58:14 +0100

    added the vercel ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: vercel()
+});
